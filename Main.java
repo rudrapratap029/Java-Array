6eb543adc8b10a740af53472c03d70aafc172aa1
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-01-29 23:27:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    static int removeDuplicates(int[] nums) {
+
+        int k = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,1,2,2,3,3,4};
+
+        int k = removeDuplicates(nums);
+
+        System.out.println("k = " + k);
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
