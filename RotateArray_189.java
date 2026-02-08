b00761156267dc5b99009240221f27f722577f05
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-02-08 21:52:02 +0530

    Add files via upload

---
+++ RotateArray_189.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class RotateArray_189 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        int n = nums.length;
+        k = k % n;
+
+        // Reverse whole array
+        reverse(nums, 0, n - 1);
+
+        // Reverse first k elements
+        reverse(nums, 0, k - 1);
+
+        // Reverse remaining elements
+        reverse(nums, k, n - 1);
+
+        System.out.println("Rotated Array: " + Arrays.toString(nums));
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+
+        while (start < end) {
+
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
