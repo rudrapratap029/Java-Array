94beecf078f5a8a948c37215c25378c29c391630
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-03-18 22:35:29 +0530

    Fix formatting issues in Missingarr.java

--- Missingarr.java
+++ Missingarr.java
@@ -2,6 +2,7 @@
 class Missingarr {
     
     public static void main(String[] args) {
+        
         int[] arr = {1, 2, 3, 4, 6};  // 5 is missing
 
         int n = 6; // last number should be 6
